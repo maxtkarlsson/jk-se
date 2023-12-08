fbbefab1e81975cbf63512c323cedad69318755e
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-12-08 14:08:24 +0100

    (feat): Add env with local API url

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { IProduct } from "../models/IProduct";
 
-const BASE_URL = "https://jk-se-backend.vercel.app/api/v1/";
-
-//"http://localhost:4000/api/v1/";
+const BASE_URL =
+  import.meta.env.VITE_REACT_LOCAL_API_URL ||
+  "https://jk-se-backend.vercel.app/api/v1/";
 
 const get = async <T>(endpoint: string) => {
   const response = await axios.get<T>(`${BASE_URL}${endpoint}`);
