4e2f7f87ddd4a88ea3416dd5ed9297f3f2860910
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-12-19 15:45:02 +0100

    (feat): Add limit and offset to fetchProducts

--- src/components/ProductsPage/ProductsPage.tsx
+++ src/components/ProductsPage/ProductsPage.tsx
@@ -1,14 +1,53 @@
+import { useState } from "react";
 import { ProductList } from "./ProductList";
-import { useFetchProductsQuery } from "./products-api-slice";
+import { useFetchProductsQuery, IProductsQuery } from "./products-api-slice";
 
 export const ProductsPage = () => {
-  const { data = [], isFetching } = useFetchProductsQuery();
+  const [limit, setLimit] = useState(0);
+  const [offset, setOffset] = useState(0);
+
+  const query: IProductsQuery = {
+    limit: limit,
+    offset: offset,
+  };
+
+  const {
+    data = { products: [], meta: {} },
+    isFetching,
+    status,
+  } = useFetchProductsQuery(query);
 
   return (
     <>
-      <div>Number of products fetched: {data.length}</div>
-      <div>isFetching: {isFetching}</div>
-      <ProductList productList={data} />
+      <div>
+        <p>Limit:</p>
+        <select
+          value={limit}
+          onChange={(e) => setLimit(Number(e.target.value))}
+        >
+          <option value="1">1</option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+        </select>
+      </div>
+      <div>
+        <p>Offset:</p>
+        <select
+          value={offset}
+          onChange={(e) => setOffset(Number(e.target.value))}
+        >
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+      </div>
+      <div>Number of products fetched: {data.products.length}</div>
+      <div>isFetching: {JSON.stringify(isFetching)}</div>
+      {status === "fulfilled" && <ProductList productList={data.products} />}
+      <div></div>
     </>
   );
 };
