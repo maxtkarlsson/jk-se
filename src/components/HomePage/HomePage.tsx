d642d9b1fcff09f571ca33fa8feecd9d2dc6c7c2
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2024-01-07 17:22:12 +0100

    (fix): Add src to images

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -5,31 +5,31 @@ export const HomePage = () => {
     <div className="home-page">
       <div className="image-1">
         <img
-          src="../../../public/1280x590/jennyprodukt26maj2016nr171.webp"
+          src="https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/1280x590/jennyprodukt26maj2016nr171.webp"
           alt="Stacked gold and silver rings with diamonds."
         />
       </div>
       <div className="image-2">
         <img
-          src="../../../public/1280x590/jennymyemma24maj2016nr608.webp"
+          src="https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/1280x590/jennymyemma24maj2016nr608.webp"
           alt="Woman wearing a silver and gold earring."
         />
       </div>
       <div className="image-3">
         <img
-          src="../../../public/1280x590/jennyverkstad20april2016nr639.webp"
+          src="https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/1280x590/jennyverkstad20april2016nr639.webp"
           alt="Torch blowing into silver bowl."
         />
       </div>
       <div className="image-4">
         <img
-          src="../../../public/1280x590/jennyprodukt26maj2016nr543.webp"
+          src="https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/1280x590/jennyprodukt26maj2016nr543.webp"
           alt="Several silver rings with different colored stones on display in a row."
         />
       </div>
       <div className="logo">
         <img
-          src="../../../public/logos/jk-se-logo.svg"
+          src="https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/logos/jk-se-logo.svg"
           alt="Jenny Kåberg Metalldesign logo"
         />
       </div>
