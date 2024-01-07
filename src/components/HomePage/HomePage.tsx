c999beb5f88e19df1960a5dab4858b4853a966a1
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2024-01-07 16:50:15 +0100

    (feat): Add styling for homepage and add media mixins in nav

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,13 +1,38 @@
-import { Button } from "../Common/Button";
-import { ButtonType } from "../../models/ButtonType";
+import "./home-page.scss";
 
 export const HomePage = () => {
   return (
-    <>
-      <h1>HomePage</h1>
-      <Button btnType={ButtonType.PRIMARY} text="Order" />
-      <Button btnType={ButtonType.SECONDARY} text="Contact me" />
-      <Button btnType={ButtonType.TERTIARY} text="Remove" />
-    </>
+    <div className="home-page">
+      <div className="image-1">
+        <img
+          src="../../../public/1280x590/jennyprodukt26maj2016nr171.webp"
+          alt="Stacked gold and silver rings with diamonds."
+        />
+      </div>
+      <div className="image-2">
+        <img
+          src="../../../public/1280x590/jennymyemma24maj2016nr608.webp"
+          alt="Woman wearing a silver and gold earring."
+        />
+      </div>
+      <div className="image-3">
+        <img
+          src="../../../public/1280x590/jennyverkstad20april2016nr639.webp"
+          alt="Torch blowing into silver bowl."
+        />
+      </div>
+      <div className="image-4">
+        <img
+          src="../../../public/1280x590/jennyprodukt26maj2016nr543.webp"
+          alt="Several silver rings with different colored stones on display in a row."
+        />
+      </div>
+      <div className="logo">
+        <img
+          src="../../../public/logos/jk-se-logo.svg"
+          alt="Jenny Kåberg Metalldesign logo"
+        />
+      </div>
+    </div>
   );
 };
