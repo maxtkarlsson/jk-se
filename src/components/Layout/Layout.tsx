669ee972a979ad3066eca4e0bfc8d5f783eb8817
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2024-01-09 09:49:20 +0100

    (feat): Add homepage link to logo in header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import "./layout.scss";
 import { Navigation } from "../Navigation/Navigation";
 
@@ -6,13 +6,15 @@ export const Layout = () => {
   return (
     <>
       <header className="header">
-        <div className="header-logo">
-          <img
-            className="header-logo__text"
-            src="https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/logos/jk-se-logo-gold.svg"
-            alt=""
-          />
-        </div>
+        <Link to={"/"}>
+          <div className="header-logo">
+            <img
+              className="header-logo__text"
+              src="https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/logos/jk-se-logo-gold.svg"
+              alt=""
+            />
+          </div>
+        </Link>
         <Navigation />
       </header>
       <main className="main">
